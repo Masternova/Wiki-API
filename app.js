4e8c9813c15801ab693cd41a20c3814fb5620598
Dillon Ellis <dillonzellis@gmail.com>
2021-09-13 16:06:38 -0400

    Refractor app routes

--- app.js
+++ app.js
@@ -16,39 +16,39 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/articles", (req, res) => {
-  Article.find((err, foundArticles) => {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
+app
+  .route("/articles")
+  .get((req, res) => {
+    Article.find((err, foundArticles) => {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save((err) => {
+      if (!err) {
+        res.send("Successfully added.");
+      } else {
+        console.log(err);
+      }
+    });
+  })
+  .delete((req, res) => {
+    Article.deleteMany((err) => {
+      if (!err) {
+        res.send("Successfully deleted");
+      } else {
+        console.log(err);
+      }
+    });
   });
-});
-
-app.post("/articles", (req, res) => {
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  newArticle.save((err) => {
-    if (!err) {
-      res.send("Successfully added.");
-    } else {
-      console.log(err);
-    }
-  });
-});
-
-app.delete("/articles", (req, res) => {
-  Article.deleteMany((err) => {
-    if (!err) {
-      res.send("Successfully deleted");
-    } else {
-      console.log(err);
-    }
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);

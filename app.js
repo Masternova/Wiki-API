f966845b21fac21995d880bfcd16f4260487bc81
Dillon Ellis <dillonzellis@gmail.com>
2021-09-13 13:31:26 -0400

    Add post and delete routes

--- app.js
+++ app.js
@@ -18,7 +18,35 @@ app.use(express.static("public"));
 
 app.get("/articles", (req, res) => {
   Article.find((err, foundArticles) => {
-    res.send(foundArticles);
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post("/articles", (req, res) => {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  newArticle.save((err) => {
+    if (!err) {
+      res.send("Successfully added.");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.delete("/articles", (req, res) => {
+  Article.deleteMany((err) => {
+    if (!err) {
+      res.send("Successfully deleted");
+    } else {
+      console.log(err);
+    }
   });
 });
 
